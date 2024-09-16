b68505ba3444c7537c59e33959fbb72c81af0fbe
JeffreySobel <jeffasobel@gmail.com>
2024-09-16 16:40:21 -0700

    FPS

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		for event in pygame.event.get():
@@ -9,6 +11,7 @@ def main():
 				return
 		screen.fill(color=000000)
 		pygame.display.flip()
+		dt = clock.tick(60)/1000
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
